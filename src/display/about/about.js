9a12d07378b58b36584f18d1f91bcdb5d0239102
chunyukuo88 <alexgochenour@gmail.com>
2022-04-30 09:36:46 -0400

    Removed ora dependency.

--- src/display/about/about.js
+++ src/display/about/about.js
@@ -1,8 +1,5 @@
-import ora from "ora";
 import { printWelcome } from "../welcome/printWelcome.js";
 import { derived } from "../../common/displayMethods.js";
-
-const spinner = ora();
 
 export async function printAboutText() {
   printWelcome();
