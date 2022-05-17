0806e34c27fda15b7a56fce77b65455f58675086
chunyukuo88 <alexgochenour@gmail.com>
2022-05-16 21:26:47 -0400

    feat: Flushed out the verbiage that is displayed when the user runs goch --help.

--- src/display/help/help.js
+++ src/display/help/help.js
@@ -1,12 +1,29 @@
-import { derived } from '../../common/displayMethods.js';
+import { derived } from "../../common/displayMethods.js";
 
 const { logYellowBox, underline } = derived;
 
-export const printHelpText = () =>  logYellowBox(`
+export const printHelpText = () =>
+  logYellowBox(`
   Gochenour Man Pages
   
   This CLI can be run with the following flags:   
         
-  $${underline('gochenour')} --about|-a      
-    This provides a brief bio of the author.            
+  $${underline("gochenour")} --about|-a      
+    This provides a brief bio of the author.
+  $${underline("gochenour")} test|t  ddt|ddi|d<integer>
+    This command, followed by any of its ensuing options, 
+    copies a piece of test suite boilerplate to your 
+    clipboard buffer.
+  $${underline("gochenour")} clean|ct
+    This command recursively removes comments, debug 
+    statements and test-specific 'describe', 'test', and
+    'it' methods from all test suites, beginning 
+    at the level of the file directory in which this 
+    command was invoked. For more details, consult the 
+    test suites in the 'cleanAllSuites' and 
+    'cleanSingleSuite' directories.
+  $${underline("gochenour")} vitest|vi
+    This command copies to your clipboard buffer a commonly
+    used string that imports Vitest methods. Handy for 
+    all Vitest suites.
 `);
--- src/display/help/help.spec.js
+++ src/display/help/help.spec.js
@@ -1,18 +1,16 @@
-import { derived } from '../../common/displayMethods.js';
-import { describe, expect, it, vi } from 'vitest';
-import { printHelpText } from './help.js';
+import { derived } from "../../common/displayMethods.js";
+import { describe, expect, it, vi } from "vitest";
+import { printHelpText } from "./help.js";
 
-vi.mock('../../common/displayMethods.js',()=>({
-  derived:  {
+vi.mock("../../common/displayMethods.js", () => ({
+  derived: {
     logYellowBox: vi.fn(),
     underline: vi.fn(),
   },
 }));
 
-describe('printHelpText()', ()=>{
-  it('This function prints a message with a yellow box and an underline.', ()=>{
-
-
+describe("printHelpText()", () => {
+  it("This function prints a message with a yellow box and an underline.", () => {
     printHelpText();
 
     expect(derived.logYellowBox).toHaveBeenCalled();
