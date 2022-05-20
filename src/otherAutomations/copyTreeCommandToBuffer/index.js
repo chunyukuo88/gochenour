2e2dd806ff0c19b45ac627c8a1239300b05cdfaa
Alex Gochenour <alexgochenour@gmail.com>
2022-05-20 16:21:31 -0400

    feat: goch tree now prints a message informing the user of the clipboard event and what to do with it.

--- src/otherAutomations/copyTreeCommandToBuffer/index.js
+++ src/otherAutomations/copyTreeCommandToBuffer/index.js
@@ -1,6 +1,9 @@
+import { derived } from '../../common/displayMethods.js';
 import clipboard from 'clipboardy';
 
 export const copyTreeCommandToBuffer = () => {
-  const expectedBufferContent = 'tree -I "node_modules|coverage|.serverless|dist" ';
   clipboard.writeSync(expectedBufferContent);
+  derived.logGreenBox(expectedBufferContent); 
 };
+
+export const expectedBufferContent = '\n The following has been copied to your clipboard buffer: \n\n   tree -I "node_modules|coverage|.serverless|dist"\n\nPress COMMAND+V to paste it into the terminal.\n';
--- src/otherAutomations/copyTreeCommandToBuffer/index.spec.js
+++ src/otherAutomations/copyTreeCommandToBuffer/index.spec.js
@@ -1,16 +1,27 @@
 import { describe, expect, test, vi } from 'vitest';
-import { copyTreeCommandToBuffer } from './index.js';
+import { copyTreeCommandToBuffer, expectedBufferContent } from './index.js';
 import clipboard from 'clipboardy';
+import { derived } from '../../common/displayMethods.js';
+
+vi.mock('../../common/displayMethods.js', ()=>({
+  derived : {
+    logGreenBox: vi.fn(),
+  }
+}));
 
 describe('copyTreeCommandToBuffer()', () => {
   describe('WHEN: this function is invoked,', () => {
     test('THEN: it copies the common tree command to the user\'s clipboard buffer.', () => {
-      const expectedBufferContent = 'tree -I "node_modules|coverage|.serverless|dist" ';
       const spy = vi.spyOn(clipboard, 'writeSync');
 
       copyTreeCommandToBuffer();
 
       expect(spy).toHaveBeenCalledWith(expectedBufferContent);
+    });
+    test('AND: it logs a message indicating that the tree command has been copied.', () => {
+      copyTreeCommandToBuffer();
+    
+      expect(derived.logGreenBox).toHaveBeenCalledWith(expectedBufferContent);
     });
   });
 });
