82b3b9e94b15d53cf18ec70a5e05890df2aaad4a
Alex Gochenour <alexgochenour@gmail.com>
2022-08-16 00:02:10 -0400

    test: Pixel conversion algorithm now has 100% coverage.

--- src/convertPixelsToRem/index.spec.js
+++ src/convertPixelsToRem/index.spec.js
@@ -1,13 +1,15 @@
-import { describe, expect, test, vi } from 'vitest';
+import { afterEach, describe, expect, test, vi } from 'vitest';
 import { convertPixelsInAllFiles } from './index.js';
 import { convertSingleFile } from './convertSingleFile.js';
 
 vi.mock('./convertSingleFile.js');
 
+afterEach(() => vi.clearAllMocks());
+
 describe('convertPixelsInAllFiles()', () => {
   describe('GIVEN: The function is invoked,', () => {
-    describe('WHEN: The current directory contains CSS files,', () => {
-      test('THEN: The pixels-to-REM conversion function is applied to each CSS file.', () => {
+    describe('WHEN: The current directory and all subdirectories contain a total of two CSS files,', () => {
+      test('THEN: The pixels-to-REM conversion function is applied to both CSS file.', () => {
         convertSingleFile.mockImplementation(vi.fn());
 
         const dir = '__test__';
@@ -15,7 +17,6 @@ describe('convertPixelsInAllFiles()', () => {
         convertPixelsInAllFiles(dir);
 
         expect(convertSingleFile).toHaveBeenCalledTimes(2);
-        vi.clearAllMocks();
       });
     });
   });
