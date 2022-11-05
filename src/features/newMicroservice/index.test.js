4488ff5b28c01d9e2de44ab6ad0924aaaab034f0
Alex Gochenour <alex.gochenour@offorhealth.com>
2022-11-05 12:40:58 -0400

    chore: Installed inquirer package and created folder called newMicroservice

---
+++ src/features/newMicroservice/index.test.js
@@ -0,0 +1,11 @@
+import { createMicroservice } from './index.js';
+
+describe('GIVEN: The createMicroservice function is invoked,', () => {
+  describe('WHEN: the user enters a name for the microservice,', () => {
+    describe('AND: a microservice of that name already exists,', () => {
+      test('THEN: the program notifies the user and stops.', () => {
+
+      });
+    });
+  });
+});
