1e99b54786043e3b9040f8bf30b496a158e37821
Alex Gochenour <alex.gochenour@offorhealth.com>
2022-11-05 12:56:35 -0400

    test(createMicroservice): Unhappy path.

--- src/features/newMicroservice/index.js
+++ src/features/newMicroservice/index.js
@@ -1,3 +1,51 @@
-export function createMicroservice(){
+import { derived } from '../../common/displayMethods.js';
+import fs from 'fs';
+import { getUserResponses } from './utils.js';
 
+export const messages = {
+  SERVICE_ALREADY_EXISTS: ' A service of that name already exists. Please try again. ',
+  SUCCESS_MESSAGE: ' Success! Service has been created. ',
+};
+
+export async function createMicroservice() {
+  const microserviceName = await getMicroserviceName();
+  const thatNameAlreadyExists = checkIfNameAlreadyExists(microserviceName);
+  console.log('haz?', thatNameAlreadyExists);
+  return (thatNameAlreadyExists)
+    ? derived.logRedBox(messages.SERVICE_ALREADY_EXISTS)
+    : derived.logGreenBox(messages.SUCCESS_MESSAGE);
 }
+
+function checkIfNameAlreadyExists(microserviceName) {
+  const entitiesInCurrentDirectory = fs.readdirSync(process.cwd());
+  let nameAlreadyExists = false;
+  for (let index in entitiesInCurrentDirectory) {
+    const entityName = entitiesInCurrentDirectory[index];
+    if (entityName === microserviceName) {
+      nameAlreadyExists = true;
+      break;
+    }
+  }
+  return nameAlreadyExists;
+}
+
+async function getMicroserviceName() {
+  let userResponses = {
+    nameOfNewService: '',
+  };
+  const queries = Object.values(queryPrompts);
+  for (const query of queries) {
+    userResponses = await getUserResponses(query, userResponses);
+  }
+  return userResponses.nameOfNewService;
+}
+
+const queryPrompts = {
+  microserviceName: [
+    {
+      type: 'input',
+      message: 'Enter a name for the new microservice:\n>',
+      name: 'nameOfNewService',
+    },
+  ],
+};
--- src/features/newMicroservice/index.test.js
+++ src/features/newMicroservice/index.test.js
@@ -1,10 +1,25 @@
-import { createMicroservice } from './index.js';
+import { createMicroservice, messages } from './index.js';
+import * as utils from './utils.js';
+import { derived } from '../../common/displayMethods.js';
+import { describe, expect, test, vi } from 'vitest';
+import fs from 'fs';
+
+vi.mock('fs');
 
 describe('GIVEN: The createMicroservice function is invoked,', () => {
   describe('WHEN: the user enters a name for the microservice,', () => {
     describe('AND: a microservice of that name already exists,', () => {
-      test('THEN: the program notifies the user and stops.', () => {
+      test('THEN: the program notifies the user and stops.', async () => {
+        const mockUserResponses = {
+          nameOfNewService: 'index.js',
+        };
+        vi.spyOn(utils, 'getUserResponses').mockImplementationOnce(() => mockUserResponses);
+        fs.readdirSync.mockImplementationOnce(() => ['index.js']);
+        const loggerSpy = vi.spyOn(derived, 'logRedBox');
 
+        await createMicroservice();
+
+        expect(loggerSpy).toBeCalledWith(messages.SERVICE_ALREADY_EXISTS);
       });
     });
   });
